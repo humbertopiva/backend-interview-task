e02b772719fe47327c5bd8bbb8f0a8c82155d28b
humbertopiva <contato.humbertopiva@gmail.com>
2025-08-28 16:35:22 -0300

    feat: implementa inicialização do banco de dados antes de iniciar o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import app from "./app";
+import { AppDataSource } from "./configs/data-source";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("📦 Conectado ao banco de dados");
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar no banco:", err);
+  });
